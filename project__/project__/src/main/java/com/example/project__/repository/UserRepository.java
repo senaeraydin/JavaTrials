5737e56c747b6a2f87da83fbd0c549c56a520911
Sena Eraydın <senaeraydin2@gmail.com>
2023-09-05 14:59:34 +0300

    Project commit one

---
+++ project__/project__/src/main/java/com/example/project__/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.project__.repository;
+
+import com.example.project__.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
